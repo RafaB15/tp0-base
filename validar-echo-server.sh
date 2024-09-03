38a51a45d94728e415d30a52083e97511e1f20e8
rafab15 <rafaelberenguel15@gmail.com>
2024-09-03 02:04:10 -0300

    Cambiando == por = para que sea npas compatible con sh

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -5,7 +5,7 @@ mensaje="Probando el echo server"
 respuesta=$(docker run --rm --network=tp0_testing_net busybox \
   sh -c "echo '$mensaje' | nc server 12345" 2>/dev/null)
 
-if [ $? -eq 0 ] && [ "$respuesta" == "$mensaje" ]; then
+if [ $? -eq 0 ] && [ "$respuesta" = "$mensaje" ]; then
   echo "action: test_echo_server | result: success"
 else
   echo "action: test_echo_server | result: fail"
