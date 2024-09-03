68450383e5aef9c76846c151595ceb205c376510
rafab15 <rafaelberenguel15@gmail.com>
2024-09-03 01:20:33 -0300

    AQuito la escritura de log lelev en el compose file

--- scripts/compose_generator.go
+++ scripts/compose_generator.go
@@ -12,21 +12,18 @@ func main() {
 
 	flag.Parse()
 
-	compose := fmt.Sprintf(`name: tp0
+	compose := `name: tp0
 services:
   server:
     container_name: server
     image: server:latest
     entrypoint: python3 /main.py
-    environment:
-      - PYTHONUNBUFFERED=%d
-      - LOGGING_LEVEL=DEBUG
     networks:
       - testing_net
     volumes:
       - ./server/config.ini:/config.ini
 
-`, *cantidad_de_clientes)
+`
 
 	for i := 1; i <= *cantidad_de_clientes; i++ {
 		nombre_cliente := fmt.Sprintf("client%d", i)
@@ -36,7 +33,6 @@ services:
     entrypoint: /client
     environment:
       - CLI_ID=%d
-      - CLI_LOG_LEVEL=DEBUG
     networks:
       - testing_net
     volumes:
